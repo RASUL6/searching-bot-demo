fe9a8882e77ac26d3bfead2fc19012a02182fb43
RASUL6 <107844693+RASUL6@users.noreply.github.com>
2022-06-20 16:00:52 +0600

    Add files via upload

---
+++ botpy — копия.py
@@ -0,0 +1,12 @@
+botimport telebot;
+bot = telebot.TeleBot('%5559285427:AAHMvI50DDywlzMTeXqqL24k-Yq1orAHNug%')
+def get_text_messages(message):
+    @bot.message_handler(content_types=['text'])
+    @bot.message_handler(content_types=['text', 'document', 'audio'])
+    if message.text == "Привет":
+    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+elif message.text == "/help":
+    bot.send_message(message.from_user.id, "Напиши привет")
+else:
+    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    bot.polling(none_stop=True, interval=0)
